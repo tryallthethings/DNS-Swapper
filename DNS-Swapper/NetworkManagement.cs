955386246a4cd1ed20a110ded6b6765ea9cf471e
Stefan <stefan.bautz@gmail.com>
2020-10-26 14:35:41 +0100

    * Moved function to swap.exe

--- DNS-Swapper/NetworkManagement.cs
+++ DNS-Swapper/NetworkManagement.cs
@@ -100,41 +100,6 @@ namespace DNS_Swapper
             }
         }
 
-        /// <summary>
-        /// Set's the DNS Server of the local machine
-        /// </summary>
-        /// <param name="NIC">NIC address</param>
-        /// <param name="DNS">DNS server address</param>
-        /// <remarks>Requires a reference to the System.Management namespace</remarks>
-        public static void setDNS(string NIC, string DNS)
-        {
-            ConnectionOptions options = PrepareOptions();
-            ManagementScope scope = PrepareScope(Environment.MachineName, options, @"\root\CIMV2");
-            ManagementPath managementPath = new ManagementPath("Win32_NetworkAdapterConfiguration");
-            ObjectGetOptions objectGetOptions = new ObjectGetOptions();
-            ManagementClass mc = new ManagementClass(scope, managementPath, objectGetOptions);
-            ManagementObjectCollection moc = mc.GetInstances();
-            foreach (ManagementObject mo in moc)
-            {
-                if ((bool)mo["IPEnabled"])
-                {
-                    if (mo["Caption"].ToString().Contains(NIC))
-                    {
-                        try
-                        {
-                            ManagementBaseObject newDNS = mo.GetMethodParameters("SetDNSServerSearchOrder");
-                            newDNS["DNSServerSearchOrder"] = DNS.Split(',');
-                            ManagementBaseObject setDNS = mo.InvokeMethod("SetDNSServerSearchOrder", newDNS, null);
-                        }
-                        catch (Exception e)
-                        {
-                            throw;
-                        }
-                    }
-                }
-            }
-        }
-
         public static IPAddress getDNS(string NICname)
         {
             NetworkInterface[] networkInterfaces = NetworkInterface.GetAllNetworkInterfaces();
